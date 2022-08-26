62297650086578fe67d2ff3cc342ef435a5aa849
Iandlley <105615079+Iandlley@users.noreply.github.com>
2022-08-26 11:03:35 -0300

    Add files via upload

--- CowClass.js
+++ CowClass.js
@@ -1,21 +1,26 @@
 "use strict";
 
-export {Cow}
+import {getCurrentDate, dateFormat, diffDates, sumDate, dateHandling, addSlashToDate} from "./dateFunctions.js";
+export {Cow};
 
 class Cow {
-    constructor(id = "", isActive = "", name = "", age = "", calvingDate = "", inseminationDate = "", milkMeasure = "", calvingPrognostic = "", teatFR = "", teatFL = "", teatBR = "", teatBL = "", diseases = ""){
+
+    constructor(id = "", isActive = "", name = "", age = "", calvingDate = "", inseminationDate = "", calvingPrognostic = "", milkMeasure = "", teatFR = [], teatFL = [], teatBR = [], teatBL = [], diseases = []){
         this.id = id;
         this.isActive = isActive;
         this.name = name;
         this.age = age;
         this.calvingDate = calvingDate;
         this.inseminationDate = inseminationDate;
-        this.milkMeasure = milkMeasure;
         this.calvingPrognostic = calvingPrognostic;
+        this.milkMeasure = milkMeasure;
         this.teatFR = teatFR;
         this.teatFL = teatFL;
         this.teatBR = teatBR;
         this.teatBL = teatBL;
         this.diseases = diseases;
-    }  
+        // this.calcCalvingPrognostic = function() {
+        //     return dateHandling(this.inseminationDate, 8);
+        // }
+    }     
 }
